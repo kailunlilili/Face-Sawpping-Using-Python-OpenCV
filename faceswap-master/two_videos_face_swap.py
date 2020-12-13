974f3e1c0b1276ea8c2ed864e53d0e68ab284d4b
eddiequ <eddieqgz@seas.upenn.edu>
2020-12-12 23:14:43 -0500

    code break down

--- faceswap-master/two_videos_face_swap.py
+++ faceswap-master/two_videos_face_swap.py
@@ -50,6 +50,107 @@ def correct_colours(im1, im2, blur_amount):
         np.uint8)
 
 
+def delaunay_triangulation(convexhull,landmarks_points):
+    rect = cv2.boundingRect(convexhull)
+    subdiv = cv2.Subdiv2D(rect)
+    subdiv.insert(landmarks_points)
+    triangles = subdiv.getTriangleList()
+    triangles = np.array(triangles, dtype=np.int32)
+    return triangles
+
+
+def get_indexes_triangles(indexes_triangles, triangles, points):
+    for t in triangles:
+        pt1 = (t[0], t[1])
+        pt2 = (t[2], t[3])
+        pt3 = (t[4], t[5])
+
+        index_pt1 = np.where((points == pt1).all(axis=1))
+        index_pt1 = extract_index_nparray(index_pt1)
+
+        index_pt2 = np.where((points == pt2).all(axis=1))
+        index_pt2 = extract_index_nparray(index_pt2)
+
+        index_pt3 = np.where((points == pt3).all(axis=1))
+        index_pt3 = extract_index_nparray(index_pt3)
+
+        if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
+            triangle = [index_pt1, index_pt2, index_pt3]
+            indexes_triangles.append(triangle)
+
+
+def line_space(tr1_pt1, tr1_pt2, tr1_pt3, lines_space_mask, im_source):
+    cv2.line(lines_space_mask, tr1_pt1, tr1_pt2, 255)
+    cv2.line(lines_space_mask, tr1_pt2, tr1_pt3, 255)
+    cv2.line(lines_space_mask, tr1_pt1, tr1_pt3, 255)
+    lines_space = cv2.bitwise_and(im_source, im_source, mask=lines_space_mask)
+    return lines_space
+
+
+def triangulation_face1(landmarks_points,triangle_index,im_source ):
+    tr1_pt1 = landmarks_points[triangle_index[0]]
+    tr1_pt2 = landmarks_points[triangle_index[1]]
+    tr1_pt3 = landmarks_points[triangle_index[2]]
+    triangle1 = np.array([tr1_pt1, tr1_pt2, tr1_pt3], np.int32)
+
+    rect1 = cv2.boundingRect(triangle1)
+    (x, y, w, h) = rect1
+    cropped_triangle = im_source[y: y + h, x: x + w]
+    cropped_tr1_mask = np.zeros((h, w), np.uint8)
+
+    points = np.array([[tr1_pt1[0] - x, tr1_pt1[1] - y],
+                       [tr1_pt2[0] - x, tr1_pt2[1] - y],
+                       [tr1_pt3[0] - x, tr1_pt3[1] - y]], np.int32)
+
+    cv2.fillConvexPoly(cropped_tr1_mask, points, 255)
+    return tr1_pt1, tr1_pt2, tr1_pt3, cropped_triangle, cropped_tr1_mask, points
+
+
+def triangulation_face2(landmarks_points2,triangle_index,im_source ):
+    tr2_pt1 = landmarks_points2[triangle_index[0]]
+    tr2_pt2 = landmarks_points2[triangle_index[1]]
+    tr2_pt3 = landmarks_points2[triangle_index[2]]
+    triangle2 = np.array([tr2_pt1, tr2_pt2, tr2_pt3], np.int32)
+
+    rect2 = cv2.boundingRect(triangle2)
+    (x, y, w, h) = rect2
+
+    cropped_tr2_mask = np.zeros((h, w), np.uint8)
+
+    points2 = np.array([[tr2_pt1[0] - x, tr2_pt1[1] - y],
+                        [tr2_pt2[0] - x, tr2_pt2[1] - y],
+                        [tr2_pt3[0] - x, tr2_pt3[1] - y]], np.int32)
+
+    cv2.fillConvexPoly(cropped_tr2_mask, points2, 255)
+    return tr2_pt2, tr2_pt2, tr2_pt3, cropped_tr2_mask, points2, rect2
+
+
+def recon_face (landmarks_points, landmarks_points2, triangle_index, img2_new_face, im_source, lines_space_mask):
+    # Triangulation of the first face
+    tr1_pt1, tr1_pt2, tr1_pt3, cropped_triangle, cropped_tr1_mask, points= triangulation_face1(landmarks_points,triangle_index,im_source)
+
+    # Lines space
+    lines_space = line_space(tr1_pt1, tr1_pt2, tr1_pt3, lines_space_mask, im_source)
+
+    # Triangulation of second face
+    tr2_pt2, tr2_pt2, tr2_pt3, cropped_tr2_mask, points2, rect2 = triangulation_face2(landmarks_points2, triangle_index, im_source)
+    (x, y, w, h) = rect2
+
+    # Warp triangles
+    points = np.float32(points)
+    points2 = np.float32(points2)
+    M = cv2.getAffineTransform(points, points2)
+    warped_triangle = cv2.warpAffine(cropped_triangle, M, (w, h))
+    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_tr2_mask)
+
+    # Reconstructing destination face
+    img2_new_face_rect_area = img2_new_face[y: y + h, x: x + w]
+    img2_new_face_rect_area_gray = cv2.cvtColor(img2_new_face_rect_area, cv2.COLOR_BGR2GRAY)
+    _, mask_triangles_designed = cv2.threshold(img2_new_face_rect_area_gray, 1, 255, cv2.THRESH_BINARY_INV)
+    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=mask_triangles_designed)
+
+    img2_new_face_rect_area = cv2.add(img2_new_face_rect_area, warped_triangle)
+    img2_new_face[y: y + h, x: x + w] = img2_new_face_rect_area
 # -----------------------------修改部分-------------------------------#
 
 def face_swap_between_image_and_video(image_path, video_path):
@@ -75,13 +176,15 @@ def face_swap_between_image_and_video(image_path, video_path):
     predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
 
-
+    # yet the mask image and detect the face
     im_source = cv2.imread(sourceImage)
     im_source = cv2.resize(im_source, (im_source.shape[1] * SCALE_FACTOR,
                                        im_source.shape[0] * SCALE_FACTOR))
     img_gray = cv2.cvtColor(im_source, cv2.COLOR_BGR2GRAY)
     source_rect = detector(im_source, 1)
     faces = detector(img_gray)
+
+
     for face in faces:
         landmarks = predictor(img_gray, face)
         landmarks_points = []
@@ -92,33 +195,9 @@ def face_swap_between_image_and_video(image_path, video_path):
 
         points = np.array(landmarks_points, np.int32)
         convexhull = cv2.convexHull(points)
-
-        # Delaunay triangulation
-        rect = cv2.boundingRect(convexhull)
-        subdiv = cv2.Subdiv2D(rect)
-        subdiv.insert(landmarks_points)
-        triangles = subdiv.getTriangleList()
-        triangles = np.array(triangles, dtype=np.int32)
-
+        triangles = delaunay_triangulation(convexhull,landmarks_points)
         indexes_triangles = []
-        for t in triangles:
-            pt1 = (t[0], t[1])
-            pt2 = (t[2], t[3])
-            pt3 = (t[4], t[5])
-
-            index_pt1 = np.where((points == pt1).all(axis=1))
-            index_pt1 = extract_index_nparray(index_pt1)
-
-            index_pt2 = np.where((points == pt2).all(axis=1))
-            index_pt2 = extract_index_nparray(index_pt2)
-
-            index_pt3 = np.where((points == pt3).all(axis=1))
-            index_pt3 = extract_index_nparray(index_pt3)
-
-            if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
-                triangle = [index_pt1, index_pt2, index_pt3]
-                indexes_triangles.append(triangle)
-
+        get_indexes_triangles(indexes_triangles, triangles,points)
 
     # video reader and writer
     cap = cv2.VideoCapture(rawVideo)
@@ -177,61 +256,7 @@ def face_swap_between_image_and_video(image_path, video_path):
         lines_space_new_face = np.zeros_like(im)
 
         for triangle_index in indexes_triangles:
-            # Triangulation of the first face
-            tr1_pt1 = landmarks_points[triangle_index[0]]
-            tr1_pt2 = landmarks_points[triangle_index[1]]
-            tr1_pt3 = landmarks_points[triangle_index[2]]
-            triangle1 = np.array([tr1_pt1, tr1_pt2, tr1_pt3], np.int32)
-
-            rect1 = cv2.boundingRect(triangle1)
-            (x, y, w, h) = rect1
-            cropped_triangle = im_source[y: y + h, x: x + w]
-            cropped_tr1_mask = np.zeros((h, w), np.uint8)
-
-            points = np.array([[tr1_pt1[0] - x, tr1_pt1[1] - y],
-                               [tr1_pt2[0] - x, tr1_pt2[1] - y],
-                               [tr1_pt3[0] - x, tr1_pt3[1] - y]], np.int32)
-
-            cv2.fillConvexPoly(cropped_tr1_mask, points, 255)
-
-            # Lines space
-            cv2.line(lines_space_mask, tr1_pt1, tr1_pt2, 255)
-            cv2.line(lines_space_mask, tr1_pt2, tr1_pt3, 255)
-            cv2.line(lines_space_mask, tr1_pt1, tr1_pt3, 255)
-            lines_space = cv2.bitwise_and(im_source, im_source, mask=lines_space_mask)
-
-            # Triangulation of second face
-            tr2_pt1 = landmarks_points2[triangle_index[0]]
-            tr2_pt2 = landmarks_points2[triangle_index[1]]
-            tr2_pt3 = landmarks_points2[triangle_index[2]]
-            triangle2 = np.array([tr2_pt1, tr2_pt2, tr2_pt3], np.int32)
-
-            rect2 = cv2.boundingRect(triangle2)
-            (x, y, w, h) = rect2
-
-            cropped_tr2_mask = np.zeros((h, w), np.uint8)
-
-            points2 = np.array([[tr2_pt1[0] - x, tr2_pt1[1] - y],
-                                [tr2_pt2[0] - x, tr2_pt2[1] - y],
-                                [tr2_pt3[0] - x, tr2_pt3[1] - y]], np.int32)
-
-            cv2.fillConvexPoly(cropped_tr2_mask, points2, 255)
-
-            # Warp triangles
-            points = np.float32(points)
-            points2 = np.float32(points2)
-            M = cv2.getAffineTransform(points, points2)
-            warped_triangle = cv2.warpAffine(cropped_triangle, M, (w, h))
-            warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_tr2_mask)
-
-            # Reconstructing destination face
-            img2_new_face_rect_area = img2_new_face[y: y + h, x: x + w]
-            img2_new_face_rect_area_gray = cv2.cvtColor(img2_new_face_rect_area, cv2.COLOR_BGR2GRAY)
-            _, mask_triangles_designed = cv2.threshold(img2_new_face_rect_area_gray, 1, 255, cv2.THRESH_BINARY_INV)
-            warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=mask_triangles_designed)
-
-            img2_new_face_rect_area = cv2.add(img2_new_face_rect_area, warped_triangle)
-            img2_new_face[y: y + h, x: x + w] = img2_new_face_rect_area
+            recon_face(landmarks_points, landmarks_points2, triangle_index, img2_new_face, im_source,lines_space_mask)
 
         # Face swapped (putting 1st face into 2nd face)
         img2_face_mask = np.zeros_like(img2_gray)
